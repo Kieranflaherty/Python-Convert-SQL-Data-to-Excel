c6ff94896903f60a7c83708c43d42b92a7f290a6
Kieran Flaherty <76489588+Kieranflaherty@users.noreply.github.com>
2022-08-10 14:35:57 +0100

    Update SQL_SCH.py

--- SQL_SCH.py
+++ SQL_SCH.py
@@ -13,19 +13,19 @@ password = 'Becton2018'
 connection = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
 
 #Setup SQL Queries
-day_l1 = "SELECT * FROM [BectonDickinson].[dbo].[ProcessDataOutput0102] where S10232_DataTimestamp >= DATEADD(DAY, -1, GETDATE()) ORDER BY S10232_DataTimestamp DESC;"
-day_l2 = "SELECT * FROM [BectonDickinson].[dbo].[ProcessDataOutput0202] where S20212_DataTimestamp >= DATEADD(DAY, -1, GETDATE()) ORDER BY S20212_DataTimestamp DESC;"
-day_l3 = "SELECT * FROM [BectonDickinson].[dbo].[ProcessDataOutput0302] where S30232_DataTimestamp >= DATEADD(DAY, -1, GETDATE()) ORDER BY S30232_DataTimestamp DESC;"
-day_l4 = "SELECT * FROM [BectonDickinson].[dbo].[ProcessDataOutput0401] where S40132_DataTimestamp >= DATEADD(DAY, -1, GETDATE()) ORDER BY S40132_DataTimestamp DESC;"
-day_l5 = "SELECT * FROM [BectonDickinson].[dbo].[ProcessDataOutput0501] where S50132_DataTimestamp >= DATEADD(DAY, -1, GETDATE()) ORDER BY S50132_DataTimestamp DESC;"
-day_l6 = "SELECT * FROM [BectonDickinson].[dbo].[ProcessDataOutput0602] where S60232_DataTimestamp >= DATEADD(DAY, -1, GETDATE()) ORDER BY S60232_DataTimestamp DESC;"
-day_l7 = "SELECT * FROM [BectonDickinson].[dbo].[ProcessDataOutput0702] where S70232_DataTimestamp >= DATEADD(DAY, -1, GETDATE()) ORDER BY S70232_DataTimestamp DESC;"
-day_l8 = "SELECT * FROM [BectonDickinson].[dbo].[ProcessDataOutput0803] where L8_DataTimestamp >= DATEADD(MONTH, -1, GETDATE()) ORDER BY L8_DataTimestamp DESC;"
+day_l1 = "SELECT * FROM [DatabaseName].[dbo] where "blah blah blah" >= DATEADD(DAY, -1, GETDATE())
+day_l2 = "SELECT * FROM [DatabaseName].[dbo] where "blah blah blah" >= DATEADD(DAY, -1, GETDATE())
+day_l3 = "SELECT * FROM [DatabaseName].[dbo] where "blah blah blah" >= DATEADD(DAY, -1, GETDATE())
+day_l4 = "SELECT * FROM [DatabaseName].[dbo] where "blah blah blah" >= DATEADD(DAY, -1, GETDATE())
+day_l5 = "SELECT * FROM [DatabaseName].[dbo] where "blah blah blah" >= DATEADD(DAY, -1, GETDATE())
+day_l6 = "SELECT * FROM [DatabaseName].[dbo] where "blah blah blah" >= DATEADD(DAY, -1, GETDATE())
+day_l7 = "SELECT * FROM [DatabaseName].[dbo] where "blah blah blah" >= DATEADD(DAY, -1, GETDATE()
+day_l8 = "SELECT * FROM [DatabaseName].[dbo] where "blah blah blah" >= DATEADD(MONTH, -1, GETDATE())
 
 cursor = connection.cursor()
 try:
     data = pd.read_sql(day_l1, connection)
-#    data.to_excel('Line_Daily_Data_'+ today.strftime('%Y_%m_%d')+'.xlsx',sheet_name='Line 1')
+    data.to_excel('Line_Daily_Data_'+ today.strftime('%Y_%m_%d')+'.xlsx',sheet_name='Line 1')
     print("Query Successful")
 except Error as err:
     print(f"Error: '{err}'")
@@ -33,7 +33,7 @@ except Error as err:
 cursor = connection.cursor()
 try:
     data2 = pd.read_sql(day_l2, connection)
-#    data2.to_csv('Line_Daily_Data_'+ today.strftime('%Y_%m_%d')+'.csv')
+    data2.to_csv('Line_Daily_Data_'+ today.strftime('%Y_%m_%d')+'.csv')
     print("Query Successful")
 except Error as err:
     print(f"Error: '{err}'")
@@ -41,7 +41,7 @@ except Error as err:
 cursor = connection.cursor()
 try:
     data3 = pd.read_sql(day_l3, connection)
-#    data.to_csv('L3_Daily_Data_'+ today.strftime('%Y_%m_%d')+'.csv')
+    data.to_csv('L3_Daily_Data_'+ today.strftime('%Y_%m_%d')+'.csv')
     print("Query Successful")
 except Error as err:
     print(f"Error: '{err}'")
@@ -49,7 +49,7 @@ except Error as err:
 cursor = connection.cursor()
 try:
     data4 = pd.read_sql(day_l4, connection)
-#    data.to_csv('L4_Daily_Data_'+ today.strftime('%Y_%m_%d')+'.csv')
+    data.to_csv('L4_Daily_Data_'+ today.strftime('%Y_%m_%d')+'.csv')
     print("Query Successful")
 except Error as err:
     print(f"Error: '{err}'")
@@ -57,7 +57,7 @@ except Error as err:
 cursor = connection.cursor()
 try:
     data5 = pd.read_sql(day_l5, connection)
-#    data.to_csv('L5_Daily_Data_'+ today.strftime('%Y_%m_%d')+'.csv')
+    data.to_csv('L5_Daily_Data_'+ today.strftime('%Y_%m_%d')+'.csv')
     print("Query Successful")
 except Error as err:
     print(f"Error: '{err}'")
@@ -65,7 +65,7 @@ except Error as err:
 cursor = connection.cursor()
 try:
     data6 = pd.read_sql(day_l6, connection)
-#    data.to_csv('L6_Daily_Data_'+ today.strftime('%Y_%m_%d')+'.csv')
+    data.to_csv('L6_Daily_Data_'+ today.strftime('%Y_%m_%d')+'.csv')
     print("Query Successful")
 except Error as err:
     print(f"Error: '{err}'")
@@ -73,7 +73,7 @@ except Error as err:
 cursor = connection.cursor()
 try:
     data7 = pd.read_sql(day_l7, connection)
-#    data.to_csv('L7_Daily_Data_'+ today.strftime('%Y_%m_%d')+'.csv')
+    data.to_csv('L7_Daily_Data_'+ today.strftime('%Y_%m_%d')+'.csv')
     print("Query Successful")
 except Error as err:
     print(f"Error: '{err}'")
@@ -81,7 +81,7 @@ except Error as err:
 cursor = connection.cursor()
 try:
     data8 = pd.read_sql(day_l8, connection)
-#    data.to_csv('L8_Daily_Data_'+ today.strftime('%Y_%m_%d')+'.csv')
+    data.to_csv('L8_Daily_Data_'+ today.strftime('%Y_%m_%d')+'.csv')
     print("Query Successful")
 except Error as err:
     print(f"Error: '{err}'")
